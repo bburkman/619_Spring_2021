fbe22c87f6276c6263bae2ffaf821af2437dd6af
Brad Burkman <bburkman@MacBook-Air.local>
2021-05-23 20:47:16 -0500

    23 May 2021

--- Accident_Analysis_and_Prevention/Anystyle.sh
+++ Accident_Analysis_and_Prevention/Anystyle.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-FILES=./Already_Read/*
+FILES="./Already_Read/*"
+rm Anystyle.bib
 touch Anystyle.bib
 for f in $FILES
 do
   echo "Extracting biblography from $f"
   # take action on each file. $f store current file name
   #  cat $f
-  anystyle -f bib find $f >> Anystyle.bib
+  anystyle -f bib find --no-layout $f >> Anystyle.bib 
+  wc Anystyle.bib
 done
